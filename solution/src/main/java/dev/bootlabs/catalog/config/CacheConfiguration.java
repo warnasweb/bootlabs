8a9dfde03d19e06a71d15a1f1a94eff1525f45be
Rajesh Warna <13366914+warnasweb@users.noreply.github.com>
2026-08-16 02:39:40 +1000

    Fix test slice and E2E dependency injection

--- solution/src/main/java/dev/bootlabs/catalog/config/CacheConfiguration.java
+++ solution/src/main/java/dev/bootlabs/catalog/config/CacheConfiguration.java
@@ -3,11 +3,13 @@ package dev.bootlabs.catalog.config;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import java.time.Duration;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableCaching
 class CacheConfiguration {
     @Bean
     CacheManager cacheManager() {
